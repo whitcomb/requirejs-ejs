bfd08d9fae4b9218f975f58ead974caee25cb2ed
Brad Whitcomb <git@bradwhitcomb.com>
2012-10-29 14:24:11 -0700

    Removed some unfinished code.

--- rejs.js
+++ rejs.js
@@ -1,37 +1,30 @@
+/** @license
+ 	* RequireJS EJS Plug-in
+ 	* Copyright(c) 2012 Brad Whitcomb <git@bradwhitcomb.com>
+ 	* MIT Licensed
+	* TODO: Documentation, Configuration, Filter Support, Build Support
+ 	*/
+
 define(['ejs', 'text'], function (ejs, text) {
 	
 	"use strict";
 	
-	var rejs, extension, buildMap;
+	var rejs, extension;
 	
 	// Change this if you want to use a different template extension.
 	extension = 'ejs';
-	
-	buildMap = [];
 	
 	rejs = {
 		
 		version: '0.1',
 		
 		load: function (name, req, onLoad, config) {
-			var index, url, template;
+			var index, url;
 			index = name.indexOf('.');
 			url = req.toUrl(name.substring(0, index) + "." + extension);
-			text.get(url, function(text) {
-				template = ejs.compile(text);
-				if (config.isBuild) {
-					buildMap[name] = template;
-				}
-				onLoad(template);
+			text.get(url, function(template) {
+				onLoad(ejs.compile(template));
 			});
-		},
-		
-		write: function (pluginName, moduleName, write) {
-			if (moduleName in buildMap) {
-			var text = jsEscape(buildMap[moduleName]);
-			write("define('" + pluginName + "!" + moduleName  +
-				"', function () { return '" + text + "';});\n");
-			}
 		}
 		
 	};
